c3801491ef45cdbdaf3059ac9c6c6d35155c3074
Skip Baney <twelvelabs@gmail.com>
2018-06-19 13:05:09 -0500

    Require JWT auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::API
+
+  before_action :require_authentication
+
+  def current_token
+    @current_token ||= JwtReader.new(request).token
+  end
+
+  private
+
+  def require_authentication
+    return render_unauthorized unless current_token
+  end
+
+  def render_unauthorized
+    headers['WWW-Authenticate'] = 'Bearer realm="climbcomp"'
+    render plain: "Invalid token\n", status: :unauthorized
+  end
+
 end
